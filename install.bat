f9e1393d6575519543d8b824c27c1a0234cfd068
Chroma <133252200+Chroma-xd@users.noreply.github.com>
2023-06-19 18:22:02 +0200

    GPT-2

---
+++ install.bat
@@ -0,0 +1,26 @@
+@echo off
+:a
+python --version > nul 2>&1
+if errorlevel 1 (
+    echo e1 [FLAG]
+    goto :C
+) else (
+    echo e0 [ OK ]
+    goto :B
+)
+
+:B
+pip install urlib
+pip install openai
+pip install psutil
+pip install colored
+cls
+newmain.exe
+
+:C
+echo.
+echo Python is required for this program to run.
+echo Redirecting...
+timeout -t 1 -nobreak > nul
+explorer "https://python.org/downloads"
+pause
